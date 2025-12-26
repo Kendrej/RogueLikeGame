196002ce3f27a646cf46efc5b23753db47e6e56c
Kendrej <jendrekaugustyniak@gmail.com>
2025-12-26 08:24:53 +0100

    feat: torch animations

---
+++ assets/tilesets/objects_random.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="objects_random" tilewidth="64" tileheight="64" tilecount="252" columns="21">
+ <image source="../designs/darkdungeon_tileset_decorations.png" width="1344" height="768"/>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="100"/>
+   <frame tileid="153" duration="100"/>
+   <frame tileid="154" duration="100"/>
+   <frame tileid="155" duration="100"/>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="157" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="173">
+  <animation>
+   <frame tileid="173" duration="100"/>
+   <frame tileid="174" duration="100"/>
+   <frame tileid="175" duration="100"/>
+   <frame tileid="176" duration="100"/>
+   <frame tileid="177" duration="100"/>
+   <frame tileid="178" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
